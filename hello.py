765511204d801228206cfa72e6e0d900c512403e
mamta081 <mamu.poly08@gmail.com>
2023-10-16 05:58:15 +0530

    please1

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello")
+import random
+
+
+nm = random.randrange(9)
+
+print("Random integers between 0 and 9: ")
+for i in range(4, 15):
+ if nm%2==0 or nm%5==0:
+	 print(nm)
+a=5
+b=5
+c=a+b
+print(c)
+
+
+print("pnmlance")
+
